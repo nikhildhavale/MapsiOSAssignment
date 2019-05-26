4a06c0dad6a6ad95c6d1502b691dad930348a207
Nikhil Vivek Dhavale <nikhil_dhavale@hotmail.com>
2019-05-26 22:09:07 +0530

    Map locations added

--- MapsiOSAssignment/MapsiOSAssignment/ViewController/MapViewController.swift
+++ MapsiOSAssignment/MapsiOSAssignment/ViewController/MapViewController.swift
@@ -8,18 +8,64 @@
 
 import UIKit
 import MapKit
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MKMapViewDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
     var trip = Trip()
     override func viewDidLoad() {
         super.viewDidLoad()
+        mapView.delegate = self
         if trip.isTripDataComplete() {
+            let startLocation = trip.startCoordinates!.coordinate
+            let endLocation = trip.endCoordinates!.coordinate
+            let startPoint = MKPointAnnotation()
+            startPoint.title = trip.startAddressName
+            startPoint.coordinate = startLocation
+            let endPoint = MKPointAnnotation()
+            endPoint.title = trip.endAddressName
+            endPoint.coordinate = endLocation
+            self.mapView.showAnnotations([startPoint,endPoint], animated: true)
+            
+            let sourcePlaceMark = MKPlacemark(coordinate: startLocation)
+            let destinationPlaceMark = MKPlacemark(coordinate: endLocation)
+            let sourceMapItem = MKMapItem(placemark: sourcePlaceMark)
+            let destinationMapItem = MKMapItem(placemark: destinationPlaceMark)
+            let directionRequest = MKDirections.Request()
+            directionRequest.source = sourceMapItem
+            directionRequest.destination = destinationMapItem
+            if trip.modesOfTravel == ModesOfTravel.driving{
+                directionRequest.transportType = .automobile
+
+            }
+            else {
+                directionRequest.transportType = .walking
+
+            }
+           let direction = MKDirections(request: directionRequest)
+            direction.calculate(completionHandler: {(response,error) in
+                guard let response = response else {
+                    return
+                }
+                if let route = response.routes.first {
+                    self.mapView.addOverlay(route.polyline, level: MKOverlayLevel.aboveRoads)
+                }
+            })
             
         }
 
     }
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let render = MKPolylineRenderer(overlay: overlay)
+        render.strokeColor = UIColor.black
+        if trip.isTripDataComplete() {
+            if trip.modesOfTravel == ModesOfTravel.walking {
+                render.lineDashPattern = [2, 5]
+            }
+        }
+        
+        return render
     
+    }
 
     /*
     // MARK: - Navigation
